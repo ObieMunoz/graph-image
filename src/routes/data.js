3fea02390d7092682fd5be1c19950f4a1e7abc8a
Obie <obiemunozjr@gmail.com>
2023-10-28 21:26:47 -0400

    Commit message: Update README.md and +page.svelte to reflect new features and credits

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,5 +1,6 @@
 export const features = [
 	'Automatically resize images according to your design specifications',
+	'Dynamically serve .webp format where supported, ensuring modern compression techniques are utilized for faster load times',
 	'Generate device-specific variants to ensure optimal download size',
 	'Prioritize initial page speed and conserve bandwidth',
 	"Employ the 'blur-up' technique or a solid background for seamless image loading experiences",
