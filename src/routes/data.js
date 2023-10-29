4dcd007efb908630ac8b84cf1830888f836627e7
Obie <obiemunozjr@gmail.com>
2023-10-29 01:31:31 -0400

    Added HD Imagery

--- src/routes/data.js
+++ src/routes/data.js
@@ -13,6 +13,369 @@ export const logo = {
 	handle: 'fONBVATVKYm2eBRtzA76'
 };
 
+export const highDefinitionAssets = [
+	{
+		"handle": "0DG2ZPDTS3C9ujRuujUY",
+		"height": 4320,
+		"size": 25882411,
+		"width": 7680
+	},
+	{
+		"handle": "E33ZTRkR0ycIckxUf8KX",
+		"height": 4320,
+		"size": 25194166,
+		"width": 7680
+	},
+	{
+		"handle": "0P3qN3IeTHi8au02Ps3j",
+		"height": 4320,
+		"size": 24655504,
+		"width": 7680
+	},
+	{
+		"handle": "RtaVQYAFRi6X4aNNgBBB",
+		"height": 4320,
+		"size": 23447264,
+		"width": 7680
+	},
+	{
+		"handle": "QEEOpvVVRtCBUO5KgeL6",
+		"height": 4320,
+		"size": 20982233,
+		"width": 7680
+	},
+	{
+		"handle": "BRN3KzK2R52XhZivUkTn",
+		"height": 4320,
+		"size": 19262651,
+		"width": 7680
+	},
+	{
+		"handle": "M2FOJKHLTSEfwzQ1LLxE",
+		"height": 4320,
+		"size": 17481703,
+		"width": 7680
+	},
+	{
+		"handle": "pfKVzhB1Slu348I7rB9V",
+		"height": 4320,
+		"size": 15859627,
+		"width": 7680
+	},
+	{
+		"handle": "8VMHu4pJSvmufhcOjtx2",
+		"height": 4320,
+		"size": 15604216,
+		"width": 7680
+	},
+	{
+		"handle": "UUFjAcCES9qsbws7iF6y",
+		"height": 4320,
+		"size": 15381843,
+		"width": 7680
+	},
+	{
+		"handle": "KHvQCp4wSp2Wk9Oi0G5E",
+		"height": 4320,
+		"size": 14858193,
+		"width": 7680
+	},
+	{
+		"handle": "sffR6RjQFCmMIgGynnqp",
+		"height": 4320,
+		"size": 14577743,
+		"width": 7680
+	},
+	{
+		"handle": "FA0WiBkTSayxs7caLhxQ",
+		"height": 4320,
+		"size": 13875879,
+		"width": 7680
+	},
+	{
+		"handle": "l9e4yEtnT6zI8mfOjyr7",
+		"height": 4320,
+		"size": 13261136,
+		"width": 7680
+	},
+	{
+		"handle": "dywRiilJS6HxRlVI5NND",
+		"height": 4320,
+		"size": 12678562,
+		"width": 7680
+	},
+	{
+		"handle": "7i2iESffQTKUuOUM8UDF",
+		"height": 4320,
+		"size": 12633028,
+		"width": 7680
+	},
+	{
+		"handle": "MnmfYlxHSL6jaEZtF3L6",
+		"height": 4320,
+		"size": 11965591,
+		"width": 7680
+	},
+	{
+		"handle": "gUoH6d6xTGXCyl6SKExg",
+		"height": 4320,
+		"size": 10991483,
+		"width": 7680
+	},
+	{
+		"handle": "XDAJ306RN2sjNv58Wzpg",
+		"height": 4320,
+		"size": 10896180,
+		"width": 7680
+	},
+	{
+		"handle": "yaAXJFTS5K6UFJS445BT",
+		"height": 4320,
+		"size": 9707570,
+		"width": 7680
+	},
+	{
+		"handle": "Qxyz0saRJ6y9WYndK9JN",
+		"height": 4320,
+		"size": 9302649,
+		"width": 7680
+	},
+	{
+		"handle": "YTuua4bWTSyhBV3igkV2",
+		"height": 4320,
+		"size": 8015454,
+		"width": 7680
+	},
+	{
+		"handle": "cO3yL6lqT8qbgOAgxlVR",
+		"height": 4320,
+		"size": 7484088,
+		"width": 7680
+	},
+	{
+		"handle": "zmIG98tSjqZLKxaI7XJa",
+		"height": 4320,
+		"size": 7030646,
+		"width": 7680
+	},
+	{
+		"handle": "G3ENqeAYTyyWvMTWlC6c",
+		"height": 4320,
+		"size": 6231032,
+		"width": 7680
+	},
+	{
+		"handle": "qP6AOAs0TY6IXWI3kYOY",
+		"height": 4320,
+		"size": 5836915,
+		"width": 7680
+	},
+	{
+		"handle": "QsvpVP9WQcWAaW0Wmb69",
+		"height": 4320,
+		"size": 5813556,
+		"width": 7680
+	},
+	{
+		"handle": "h4LRTu4ScG7iRow8F7UZ",
+		"height": 4320,
+		"size": 5484643,
+		"width": 7680
+	},
+	{
+		"handle": "BoilBf2QRBn39qHs75nA",
+		"height": 4320,
+		"size": 5199319,
+		"width": 7680
+	},
+	{
+		"handle": "EjxsxR7iRHmWgwRb7Le1",
+		"height": 4320,
+		"size": 4914362,
+		"width": 7680
+	},
+	{
+		"handle": "QzRXvBdUQu2rrIRSNK1Q",
+		"height": 4320,
+		"size": 4605722,
+		"width": 7680
+	},
+	{
+		"handle": "fOSKOHPQrOoMlUJZYgJw",
+		"height": 4320,
+		"size": 4258278,
+		"width": 7680
+	},
+	{
+		"handle": "4qsXpXoyRheVGygcvFjm",
+		"height": 4320,
+		"size": 4109191,
+		"width": 7680
+	},
+	{
+		"handle": "Q1eOI22MRI2PGZB7NZ3K",
+		"height": 4320,
+		"size": 3929516,
+		"width": 7680
+	},
+	{
+		"handle": "lvKwFIsR06IKOUE6zFAS",
+		"height": 4320,
+		"size": 3884444,
+		"width": 7680
+	},
+	{
+		"handle": "6rjlqvr0TbiWVpXaCJCZ",
+		"height": 4320,
+		"size": 3848278,
+		"width": 7680
+	},
+	{
+		"handle": "pLixNNk7SEeDohyzRmcf",
+		"height": 4320,
+		"size": 3846450,
+		"width": 7680
+	},
+	{
+		"handle": "0lBCh8XSSWaIqpxcgXHQ",
+		"height": 4320,
+		"size": 3672932,
+		"width": 7680
+	},
+	{
+		"handle": "12xWhHvQEGDX4rteMcDG",
+		"height": 4320,
+		"size": 3663890,
+		"width": 7680
+	},
+	{
+		"handle": "JjqfF4qSHy0drmLKxrX6",
+		"height": 4320,
+		"size": 3475418,
+		"width": 7680
+	},
+	{
+		"handle": "GtSyoUKRTyyZS5stfnqh",
+		"height": 4320,
+		"size": 3401342,
+		"width": 7680
+	},
+	{
+		"handle": "jIjmR7jIT7KIjHKTUODz",
+		"height": 4320,
+		"size": 3296965,
+		"width": 7680
+	},
+	{
+		"handle": "wBcHgK7JTfmw1jaMS5BR",
+		"height": 4320,
+		"size": 3208318,
+		"width": 7680
+	},
+	{
+		"handle": "FKF4T0eLTZmtwU5BeyeU",
+		"height": 4320,
+		"size": 3121026,
+		"width": 7680
+	},
+	{
+		"handle": "3kd7wlVQ4KAcGtgIdChs",
+		"height": 4320,
+		"size": 3021608,
+		"width": 7680
+	},
+	{
+		"handle": "WnKtDEcUSz2HSYl9qtBa",
+		"height": 4320,
+		"size": 2902969,
+		"width": 7680
+	},
+	{
+		"handle": "jWOzr82fQPGeSKj4aOcq",
+		"height": 4320,
+		"size": 2889908,
+		"width": 7680
+	},
+	{
+		"handle": "x0wKOXTEQNeuKvv7MSAk",
+		"height": 4320,
+		"size": 2855961,
+		"width": 7680
+	},
+	{
+		"handle": "fu4OoNS5SEyIa7O6pWxt",
+		"height": 4320,
+		"size": 2693841,
+		"width": 7680
+	},
+	{
+		"handle": "S1r24jLRT7NUkdrXE97t",
+		"height": 4320,
+		"size": 2658120,
+		"width": 7680
+	},
+	{
+		"handle": "OsJZhriFT9WjMw7FjjMr",
+		"height": 4320,
+		"size": 2548323,
+		"width": 7680
+	},
+	{
+		"handle": "Nnw9K7DsQcm10qHFL6QW",
+		"height": 4320,
+		"size": 2459498,
+		"width": 7680
+	},
+	{
+		"handle": "6GX9uOLiTqmb5IjaSlck",
+		"height": 4320,
+		"size": 2133137,
+		"width": 7680
+	},
+	{
+		"handle": "XQsLVLeQbqssBaHmpbsm",
+		"height": 4320,
+		"size": 2125588,
+		"width": 7680
+	},
+	{
+		"handle": "iiwz8Wa5S1ODMLwEJq8h",
+		"height": 4320,
+		"size": 2124117,
+		"width": 7680
+	},
+	{
+		"handle": "5qfC9170Q1iqSOKd5M7Z",
+		"height": 4320,
+		"size": 1994978,
+		"width": 7680
+	},
+	{
+		"handle": "4EBZNlATQmyN3VDsCzwl",
+		"height": 4320,
+		"size": 1637535,
+		"width": 7680
+	},
+	{
+		"handle": "tY3aCuf3QaaZi2iuTQYp",
+		"height": 4320,
+		"size": 1527731,
+		"width": 7680
+	},
+	{
+		"handle": "RtXq5qjURfu7zumEgmDm",
+		"height": 4320,
+		"size": 1324893,
+		"width": 7680
+	},
+	{
+		"handle": "IQdo7QzZSXyM6DjVR6MR",
+		"height": 4320,
+		"size": 516750,
+		"width": 7680
+	}
+]
+
 export const assets = [
 	{
 		height: 1280,
